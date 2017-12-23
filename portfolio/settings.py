049f58cab8de9b078f3dd36b8d90aecdc597cef2
Erick <rick.mutua12@gmail.com>
2017-12-23 21:37:17 +0300

    deployment to heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['rick-mutua.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['rick-mutua.herokuapp.com', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
@@ -88,6 +89,9 @@ DATABASES = {
         'PASSWORD':'qwerty12345',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
