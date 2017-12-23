eb074c782bc885f3239ede645b3e2aad68d6847c
Erick <rick.mutua12@gmail.com>
2017-12-23 21:13:06 +0300

    deployment to heroku

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export GDAL_LIBRARY_PATH="/Library/Frameworks/GDAL.framework/Versions/2.1.0/GDAL"
-export GEOS_LIBRARY_PATH="/Library/Frameworks/GEOS.framework/Versions/3/GEOS"
