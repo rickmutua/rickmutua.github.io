c9063163793a68a4129b2f4e83b4dd6ee4427f6d
Erick <rick.mutua12@gmail.com>
2017-12-23 17:00:51 +0300

    minor modifications and updates

--- rick/admin.py
+++ rick/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Tag, Project
 
 # Register your models here.
+
+
+class ProjectTag(admin.ModelAdmin):
+
+    filter_horizontal = ('tags',)
+
+
+admin.site.register(Tag)
+admin.site.register(Project)
