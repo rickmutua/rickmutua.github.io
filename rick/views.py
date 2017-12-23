b3238213f1c73da8a8662601bf9b060598c81558
Erick <rick.mutua12@gmail.com>
2017-12-23 16:56:28 +0300

    definen contact, project and project info.

--- rick/views.py
+++ rick/views.py
@@ -1,10 +1,53 @@
 from django.shortcuts import render
+from .models import Tag, Project
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 
 # Create your views here.
 
 
 def index(request):
 
-    title = 'Wankers'
+    title = 'Web Portfolio | Erick Mutua'
 
     return render(request, 'index.html', {'title': title})
+
+
+def project(requests):
+
+    try:
+
+        tags = Tag.objects.filter().all()
+
+        projects = Project.objects.filter().all().order_by('-id')
+
+        return render(requests, 'projects/project.html', {'tags': tags, 'projects': projects})
+
+    except ObjectDoesNotExist:
+
+        raise Http404()
+
+
+def single_project(requests, project_name):
+
+    try:
+
+        project = Project.objects.get(name=project_name)
+
+        return render(requests, 'projects/project-info.html', {'project': project})
+
+    except ObjectDoesNotExist:
+
+        raise Http404()
+
+
+def contact(request):
+
+    try:
+
+        return render(request, 'contact.html')
+
+    except ObjectDoesNotExist:
+
+        raise Http404()
